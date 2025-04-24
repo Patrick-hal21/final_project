efccc0bc588890d39e7fb34cc92e76a11d09caf5
My name <Saturnking2015@gmail.com>
2025-04-24 19:56:19 +0630

    Made some modifications ,and changed file

--- static/js/landing_pg.js
+++ static/js/landing_pg.js
@@ -1,4 +1,21 @@
-document.getElementById("to_nav_bar").onclick = () => document.getElementById("navbar").scrollIntoView({ behavior: 'smooth' });
+const button = document.getElementById("to_nav_bar");
+
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
+    const pageHeight = document.body.scrollHeight;
+    const oneThirdHeight = pageHeight / 3;
+
+    // Show button after scrolling 1/3 of the page
+    if (scrollPosition > oneThirdHeight) {
+        button.classList.remove("opacity-0", "pointer-events-none");
+        button.classList.add("opacity-100");
+    } else {
+        button.classList.remove("opacity-100");
+        button.classList.add("opacity-0", "pointer-events-none");
+    }
+});
+      
+button.onclick = () => document.getElementById("navbar").scrollIntoView({ behavior: 'smooth' });
 
 document.querySelectorAll(".toLogin").forEach(item => {
     item.onclick = () => window.location.href = "./login_email.html";
