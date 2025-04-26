6284744db8bd3e896590710f221a9b54d152c71f
My name <Saturnking2015@gmail.com>
2025-04-26 12:00:16 +0630

    Modified Responsiveness

--- static/js/landing_pg.js
+++ static/js/landing_pg.js
@@ -1,21 +1,4 @@
-const button = document.getElementById("to_nav_bar");
 
-window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    const pageHeight = document.body.scrollHeight;
-    const oneThirdHeight = pageHeight / 3;
-
-    // Show button after scrolling 1/3 of the page
-    if (scrollPosition > oneThirdHeight) {
-        button.classList.remove("opacity-0", "pointer-events-none");
-        button.classList.add("opacity-100");
-    } else {
-        button.classList.remove("opacity-100");
-        button.classList.add("opacity-0", "pointer-events-none");
-    }
-});
-      
-button.onclick = () => document.getElementById("navbar").scrollIntoView({ behavior: 'smooth' });
 
 // slide show
 let currentIndex = 0;
