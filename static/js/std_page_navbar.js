e65039a7e48b67ed38dd8c3c717481095236479d
My name <Saturnking2015@gmail.com>
2025-05-06 17:12:13 +0630

    Refactored std_page.js and Added Notifications dropdown in system_page_std.html

--- static/js/std_page_navbar.js
+++ static/js/std_page_navbar.js
@@ -5,27 +5,41 @@ humburgerBtn.onclick = function() {
     dropElement.classList.toggle('dropdown-visible');
 }
 
+
+function toggle_dropdown(btn, box) {
+  btn.addEventListener('click', function (event) {
+    event.stopPropagation(); // Prevent this click from bubbling to document
+    box.classList.toggle('opacity-0');
+    box.classList.toggle('opacity-100');
+  
+    btn.querySelector('svg').classList.toggle('opacity-100'); // toggle noti's opacity
+  });
+  
+  document.addEventListener('click', function (event) {
+    if (!btn.contains(event.target) && !box.contains(event.target)) {
+      if (box.classList.contains('opacity-100')) {
+        box.classList.remove('opacity-100');
+        box.classList.add('opacity-0');
+  
+        btn.querySelector('svg').classList.toggle('opacity-100'); // toggle noti's opacity
+      }
+    }
+  });
+}
+
 const profile_card = document.getElementById('profile_box');
 const profile = document.getElementById('profile');
 
-profile.addEventListener('click', function (event) {
-    event.stopPropagation(); // Prevent this click from bubbling to document
-    profile_card.classList.toggle('opacity-0');
-    profile_card.classList.toggle('opacity-100');
-});
+const noti = document.getElementById('noti');
+const noti_box = document.getElementById('noti_box');
 
-document.addEventListener('click', function (event) {
-    if (!profile.contains(event.target) && !profile_card.contains(event.target)) {
-      if (profile_card.classList.contains('opacity-100')) {
-        profile_card.classList.remove('opacity-100');
-        profile_card.classList.add('opacity-0');
-      }
-    }
-});
+toggle_dropdown(profile, profile_card);
+toggle_dropdown(noti, noti_box);
 
 
+
+// switch tab
 const allTabSections = document.querySelectorAll(".navbar-tab");
-
 allTabSections.forEach(navTab => {
   const tabs = Array.from(navTab.querySelectorAll("a"));
   
@@ -74,37 +88,29 @@ allTabSections.forEach(navTab => {
 
 
 // button gif animation
+function hover_gif_animate(btn, img, src1, src2){
+  btn.addEventListener('mouseenter', function (event) {
+    // event.stopPropagation(); // Prevent this click from bubbling to document
+      img.src = src1; // first half of the gif which is taking seat
+  });
+  
+  btn.addEventListener('mouseleave', function (event) {
+    // event.stopPropagation(); // Prevent this click from bubbling to document
+      img.src = src2; // second half of the gif which is placing seat
+  });
+}
+
 const join_class_btn = document.getElementById('join_class');
 const take_seat = join_class_btn.children[0];
 
+hover_gif_animate(join_class_btn, take_seat, './static/images/take_seat.gif', './static/images/place_seat.gif');
+
 const leave_form_btn = document.getElementById('leave_form');
 const leave_form = leave_form_btn.children[0];
-
-join_class_btn.addEventListener('mouseenter', function (event) {
-  // event.stopPropagation(); // Prevent this click from bubbling to document
-    take_seat.src = './static/images/take_seat.gif'; // first half of the gif which is taking seat
-
-    // take_seat.src = './static/images/classroom_nobg.gif'; // full gif
-});
-
-join_class_btn.addEventListener('mouseleave', function (event) {
-  // event.stopPropagation(); // Prevent this click from bubbling to document
-    take_seat.src = './static/images/place_seat.gif'; // second half of the gif which is placing seat
-
-    // take_seat.src = './static/images/seat.png'; // default png
-});
-
-leave_form_btn.addEventListener('mouseenter', function (event) {
-  // event.stopPropagation(); // Prevent this click from bubbling to document
-    leave_form.src = './static/images/form.gif'; // first half of the gif which is taking seat
-});
-
-leave_form_btn.addEventListener('mouseleave', function (event) {
-  // event.stopPropagation(); // Prevent this click from bubbling to document
-    leave_form.src = './static/images/form.png'; // second half of the gif which is placing seat
-});
+hover_gif_animate(leave_form_btn, leave_form, './static/images/form.gif', './static/images/form.png')
 
 
+// slide-in div onload
 document.addEventListener("DOMContentLoaded", () => {
   const target = document.getElementById("slideInElement");
   
@@ -116,4 +122,35 @@ document.addEventListener("DOMContentLoaded", () => {
   }, { threshold: window.innerWidth < 768 ? 0.08 : 0.1 });
 
   observer.observe(target);
+  
 });
+
+
+// mark as all read on hover
+
+const mark_as_all_btn = document.getElementById("mark-as-all");
+const mark_read_p = mark_as_all_btn.querySelector("p");
+
+
+// btn = btn or any other element to add hover event
+// text - p or text element to show on hovering 
+function show_text_on_hover(btn, text_e) {
+  let timeoutId;
+  btn.addEventListener("mouseenter", () => {
+    // Start 1.5s delay to show tooltip
+    timeoutId = setTimeout(() => {
+      text_e.classList.remove("hidden");
+    }, 1500);
+  });
+  
+  btn.addEventListener("mouseleave", () => {
+    // Cancel the pending timeout
+    clearTimeout(timeoutId);
+    
+    // Optionally hide the tooltip immediately
+    text_e.classList.add("hidden");
+  });
+}
+
+show_text_on_hover(mark_as_all_btn, mark_read_p);
+
